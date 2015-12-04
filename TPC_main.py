c8274638ada05091f51d9aac4311d2bb3bbdbdb6
Rohit Suratekar <rohitsuratekar@gmail.com>
2015-12-04 22:22:33 +0530

    Initial Commit

---
+++ TPC_main.py
@@ -0,0 +1,9 @@
+#Main code: December 2015
+#General use code
+#main code file
+#Initial Imports
+import TPC_settings as tset
+import math
+import numpy as np
+from scipy.integrate import odeint
+import matplotlib
